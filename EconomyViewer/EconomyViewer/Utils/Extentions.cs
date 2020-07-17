12c0c37c25514d2493f4fe3084c08e4b381b48f0
AquaVG <vladd.golovatyuk@gmail.com>
2020-07-17 11:46:33 +0300

    Extentions class to easy up coding

---
+++ EconomyViewer/EconomyViewer/Utils/Extentions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace EconomyViewer.Utils
+{
+    static class Extentions
+    {
+        public static void ClearSelection(this ComboBox comboBox)
+        {
+            TextBox textBox = comboBox.Template.FindName("PART_EditableTextBox", comboBox) as TextBox;
+            int index = textBox.Text.Length;
+            textBox.SelectionLength = 0;
+            textBox.CaretIndex = index;
+        }
+        public static bool IsOneOf<T>(this T obj, params T[] elems)
+        {
+            return elems.Contains(obj);
+        }
+        public enum MapType : uint
+        {
+            MAPVK_VK_TO_VSC = 0x0,
+            MAPVK_VSC_TO_VK = 0x1,
+            MAPVK_VK_TO_CHAR = 0x2,
+            MAPVK_VSC_TO_VK_EX = 0x3,
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int ToUnicode(
+            uint wVirtKey,
+            uint wScanCode,
+            byte[] lpKeyState,
+            [Out, MarshalAs(UnmanagedType.LPWStr, SizeParamIndex = 4)]
+            StringBuilder pwszBuff,
+            int cchBuff,
+            uint wFlags);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetKeyboardState(byte[] lpKeyState);
+
+        [DllImport("user32.dll")]
+        public static extern uint MapVirtualKey(uint uCode, MapType uMapType);
+
+        public static char GetChar(this Key key)
+        {
+            char ch = ' ';
+
+            int virtualKey = KeyInterop.VirtualKeyFromKey(key);
+            byte[] keyboardState = new byte[256];
+            GetKeyboardState(keyboardState);
+
+            uint scanCode = MapVirtualKey((uint)virtualKey, MapType.MAPVK_VK_TO_VSC);
+            StringBuilder stringBuilder = new StringBuilder(2);
+
+            int result = ToUnicode((uint)virtualKey, scanCode, keyboardState, stringBuilder, stringBuilder.Capacity, 0);
+
+            if (result.IsOneOf(-1, 0) == false)
+                ch = stringBuilder[0];
+            return ch;
+        }
+    }
+}
