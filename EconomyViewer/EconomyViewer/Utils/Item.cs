9f75f4e0e6551c6a67040f68b164a99cfec2fb13
AquaVG <vladd.golovatyuk@gmail.com>
2020-07-17 11:42:58 +0300

    Item class and ItemList

---
+++ EconomyViewer/EconomyViewer/Utils/Item.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace EconomyViewer.Utils
+{
+    /// <summary>
+    /// Класс представление для всех предметов экономики SMC.
+    /// </summary>
+    internal class Item : INotifyPropertyChanged
+    {
+        private string header;
+        private int count;
+        private decimal price;
+        private string mod;
+
+        /// <summary>
+        /// ID предмета в базе данных. Доступ только для чтения
+        /// </summary>
+        public int ID { get; private set; }
+        /// <summary>
+        /// Название предмета.
+        /// </summary>
+        public string Header
+        {
+            get => header;
+
+            set {
+                header = value;
+                OnPropertyChanged("Header");
+            }
+        }
+        /// <summary>
+        /// Количество предмета за стоимость <see cref="Price"/>.
+        /// </summary>
+        public int Count
+        {
+            get => count;
+
+            set {
+                count = value;
+                OnPropertyChanged("Count");
+            }
+        }
+        /// <summary>
+        /// Стоимость предмета в количестве <see cref="Count"/>.
+        /// </summary>
+        public decimal Price
+        {
+            get => price;
+
+            set {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        /// <summary>
+        /// Модификация, которая предоставляет данный предмет.
+        /// </summary>
+        public string Mod
+        {
+            get => mod;
+            set {
+                mod = value;
+                OnPropertyChanged("Mod");
+            }
+        }
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="Item"/> c полной задачей параметров.
+        /// </summary>
+        /// <param name="id">ID предмета.</param>
+        /// <param name="header">Название предмета.</param>
+        /// <param name="count">Количество предмета.</param>
+        /// <param name="price">Стоимость предмета за его количество.</param>
+        /// <param name="mod">Модификация предмета.</param>
+        public Item(int id, string header, int count, int price, string mod)
+        {
+            ID = id;
+            this.header = header;
+            this.count = count;
+            this.price = Convert.ToDecimal(price);
+            this.mod = mod;
+        }
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="Item"/> c задачей открытых параметров.
+        /// </summary>
+        /// <param name="header">Название предмета.</param>
+        /// <param name="count">Количество предмета.</param>
+        /// <param name="price">Стоимость предмета за его количество.</param>
+        /// <param name="mod">Модификация предмета.</param>
+        public Item(string header, int count, int price, string mod)
+        {
+            this.header = header ?? throw new ArgumentNullException("Header value equals null");
+            this.count = count;
+            this.price = Convert.ToDecimal(price);
+            this.mod = mod ?? throw new ArgumentNullException("Mod value equals null");
+        }
+        /// <summary>
+        /// Инициализирует новый экземпляр <see cref="Item"/> со стандартными параметрами.
+        /// </summary>
+        public Item() { }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+        /// <summary>
+        /// Возвращает строку, представляющую данный <see cref="Item"/>
+        /// </summary>
+        /// <returns>Строку в формате "Название Количество шт. - Цена"</returns>
+        public override string ToString()
+        {
+            return $"{header} {count} шт. - {price}";
+        }
+        public override bool Equals(object obj)
+        {
+            Item item = (Item)obj;
+            return item.header == header && item.count == count && item.price == price && item.mod == mod;
+        }
+        public static Item FromString(string value, string mod)
+        {
+            if (Regex.IsMatch(value, "(.+) ([0-9]+) шт. - ([0-9]+)$"))
+            {
+                string itemName = Regex.Match(value, @"(.+) ([0-9]+) шт. - ([0-9]+)$").Groups[1].Value;
+                int itemCount = Convert.ToInt32(Regex.Match(value, @"(.+) ([0-9]+) шт. - ([0-9]+)$").Groups[2].Value);
+                int itemPrice = Convert.ToInt32(Regex.Match(value, @"(.+) ([0-9]+) шт. - ([0-9]+)$").Groups[3].Value);
+                string itemMod = mod;
+
+                Item item = new Item(itemName, itemCount, itemPrice, itemMod);
+                return item;
+            }
+            return null;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
---
+++ EconomyViewer/EconomyViewer/Utils/ItemList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EconomyViewer.Utils
+{
+    internal class ItemList<T> : ICollection<Item> where T : Item
+    {
+        private readonly List<Item> itemList = new List<Item>();
+        public int Count { get => itemList.Count; }
+        public bool IsReadOnly { get; }
+        public delegate void DataChangedHandler();
+        public event DataChangedHandler OnDataChanged;
+        public void Add(Item item)
+        {
+            if (itemList.Contains(item))
+            {
+                itemList.Remove(item);
+                Item newItem = new Item()
+                {
+                    Count = item.Count + item.Count,
+                    Price = item.Price + item.Price,
+                    Header = item.Header,
+                    Mod = item.Mod
+                };
+                itemList.Add(newItem);
+            }
+            else if (itemList.Any(i => i.Header == item.Header))
+            {
+                Item sameItem = itemList.Find(i => i.Header == item.Header);
+                if (sameItem.Price / sameItem.Count == item.Price / item.Count)
+                {
+                    int index = itemList.IndexOf(sameItem);
+                    itemList.RemoveAt(index);
+                    itemList.Insert(index, new Item()
+                    {
+                        Header = sameItem.Header,
+                        Count = sameItem.Count + item.Count,
+                        Price = sameItem.Price + item.Price,
+                        Mod = sameItem.Mod
+                    });
+                }
+            }
+            else itemList.Add(item);
+            OnDataChanged?.Invoke();
+        }
+
+        public void Clear()
+        {
+            itemList.Clear();
+            OnDataChanged?.Invoke();
+        }
+
+        public bool Contains(Item item)
+        {
+            return itemList.Contains(item);
+        }
+
+        public void CopyTo(Item[ ] array, int arrayIndex)
+        {
+            itemList.CopyTo(array, arrayIndex);
+        }
+
+        public IEnumerator<Item> GetEnumerator()
+        {
+            return itemList.GetEnumerator();
+        }
+
+        public bool Remove(Item item)
+        {
+            if (itemList.Contains(item))
+            {
+                itemList.Remove(item);
+                OnDataChanged?.Invoke();
+                return true;
+            }
+            else if (itemList.Any(i => i.Header == item.Header))
+            {
+                Item sameItem = itemList.Find(i => i.Header == item.Header);
+                if (sameItem.Price / sameItem.Count == item.Price / item.Count)
+                {
+                    int index = itemList.IndexOf(sameItem);
+                    itemList.RemoveAt(index);
+                    itemList.Insert(index, new Item()
+                    {
+                        Header = sameItem.Header,
+                        Count = sameItem.Count - item.Count,
+                        Price = sameItem.Price - item.Price,
+                        Mod = sameItem.Mod
+                    });
+                    OnDataChanged?.Invoke();
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
