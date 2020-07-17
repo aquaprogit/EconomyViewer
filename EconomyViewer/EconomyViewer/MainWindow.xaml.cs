f91bdcdd9fd624d93ce41335543b022e26a7e79c
AquaVG <vladd.golovatyuk@gmail.com>
2020-07-17 16:49:21 +0300

    Changing Data base worker from access to SQLite

--- EconomyViewer/EconomyViewer/MainWindow.xaml.cs
+++ EconomyViewer/EconomyViewer/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +10,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using EconomyViewer.Utils;
 
 namespace EconomyViewer
 {
@@ -35,6 +39,25 @@ namespace EconomyViewer
                 toolTipHome
             };
             toolTips.ForEach(c => c.Visibility = ToggleNavBar_ToggleButton.IsChecked.Value ? Visibility.Collapsed : Visibility.Visible);
+        }
+        private void WorkingPanel_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ToggleNavBar_ToggleButton.IsChecked = false;
+        }
+        #endregion
+        #region Window controls
+        private void MinimizeWindow_Button_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized; 
+        }
+
+        private void CloseWindow_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
         #endregion
     }
--- EconomyViewer/EconomyViewer/Utils/DataBaseWorker.cs
+++ EconomyViewer/EconomyViewer/Utils/DataBaseWorker.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Data;
-using System.Data.OleDb;
+using System.Data.SQLite;
 using System.Diagnostics;
 
 namespace EconomyViewer.Utils
 {
     internal static class DataBaseWorker
     {
-        private static readonly string connectionString = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source="
+        private static readonly string connectionString = @"Data Source="
             + System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName)
-            + "\\economy.mdb";
-        private static OleDbConnection connection;
+            + "\\economy.db; Version=3;";
+        private static SQLiteConnection connection;
 
         public static void DeleteData(string serverName, Item toDelete)
         {
@@ -18,14 +18,15 @@ namespace EconomyViewer.Utils
                 $"WHERE i_id={toDelete.ID}");
         }
 
-        public static void DeleteAddData(string serverName)
+        public static void DeleteAllData(string serverName)
         {
             ExecuteCommand($"DELETE FROM {serverName}");
+
         }
         public static List<string> GetAllTables()
         {
             DataTable userTables = null;
-            using (connection = new OleDbConnection(connectionString))
+            using (connection = new SQLiteConnection(connectionString))
             {
                 string[] restrictions = new string[4];
                 restrictions[3] = "Table";
@@ -39,14 +40,14 @@ namespace EconomyViewer.Utils
         }
         public static List<Item> GetData(string serverName)
         {
-            using (connection = new OleDbConnection(connectionString))
+            using (connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
                 List<Item> result = new List<Item>();
 
                 string query = $"SELECT * FROM {serverName} ORDER BY i_mod, i_header";
-                OleDbCommand command = new OleDbCommand(query, connection);
-                OleDbDataReader reader = command.ExecuteReader();
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                SQLiteDataReader reader = command.ExecuteReader();
                 while (reader.Read())
                 {
                     result.Add(new Item(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2), reader.GetInt32(3), reader.GetString(4)));
@@ -57,14 +58,14 @@ namespace EconomyViewer.Utils
         }
         public static List<string> GetOnlyColumnList(string serverName, string collumnName)
         {
-            using (connection = new OleDbConnection(connectionString))
+            using (connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
                 List<string> result = new List<string>();
 
                 string query = $"SELECT DISTINCT {collumnName} FROM {serverName}";
-                OleDbCommand command = new OleDbCommand(query, connection);
-                var reader = command.ExecuteReader();
+                SQLiteCommand command = new SQLiteCommand(query, connection);
+                SQLiteDataReader reader = command.ExecuteReader();
                 while (reader.Read())
                 {
                     result.Add(reader.GetString(0));
@@ -83,12 +84,12 @@ namespace EconomyViewer.Utils
                 $"SET i_header='{updatedItem.Header}', i_count={updatedItem.Count}, i_price={updatedItem.Price}, i_mod='{updatedItem.Mod}' " +
                 $"WHERE i_id={id}");
         }
-        private static void ExecuteCommand(string query)
+        public static void ExecuteCommand(string query)
         {
-            using (connection = new OleDbConnection(connectionString))
+            using (connection = new SQLiteConnection(connectionString))
             {
                 connection.Open();
-                OleDbCommand command = new OleDbCommand(query, connection);
+                SQLiteCommand command = new SQLiteCommand(query, connection);
                 command.ExecuteNonQuery();
             }
         }
